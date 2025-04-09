1ec03cd9afcc8ca4334241a78b2071db084e935a
kmoreaux <depaninfo54@gmail.com>
2025-04-09 13:41:03 +0200

    modified:   src/components/Apropos.jsx

--- src/components/Apropos.jsx
+++ src/components/Apropos.jsx
@@ -8,26 +8,24 @@ const Apropos = () => {
       <Row className="g-4">
         <Col md={6}>
           <Card className="h-100">
-            <Card.Body>
-              <Card.Title>Mon Parcours</Card.Title>
-              <Card.Text>
+            <Card.Body className="d-flex flex-column">
+              <Card.Title className="mb-3">Mon Parcours</Card.Title>
+              <Card.Text className="flex-grow-1">
                 Passionné d'informatique depuis mon plus jeune âge, j'ai développé des compétences variées dans le domaine du développement et les nouvelles technologies.
               </Card.Text>
-              
             </Card.Body>
           </Card>
         </Col>
 
         <Col md={6}>
           <Card className="h-100">
-            <Card.Body>
-            <Card.Title className="mt-4">Mes Valeurs et qualités</Card.Title>
-              <ul className="list-unstyled">
+            <Card.Body className="d-flex flex-column">
+              <Card.Title className="mb-3">Mes Valeurs et qualités</Card.Title>
+              <ol className="list-unstyled flex-grow-1">
                 <li>• Innovation</li>
                 <li>• Adaptation aux changements</li>
                 <li>• Partage de connaissances</li>
-              </ul>
-              
+              </ol>
             </Card.Body>
           </Card>
         </Col>
